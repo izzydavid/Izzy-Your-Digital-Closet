586d934041460be689960d8cff91cabbcf4258ce
IzzyDavid <david.irizarry86@yahoo.com>
2019-10-14 04:46:11 -0400

    test

--- server.js
+++ server.js
@@ -4,24 +4,21 @@ var bodyParser = require("body-parser");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
-//
+
+var sessionStore = new session.MemoryStore();
+
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 //
 // Creating express app and configuring middleware needed for authentication
 var app = express();
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 // We need to use sessions to keep track of our user's login status
-app.use(session({
-  secret: "keyboard cat",
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true, store: sessionStore }));
 app.use(passport.initialize());
 app.use(passport.session());
 //
@@ -30,8 +27,8 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 //
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
